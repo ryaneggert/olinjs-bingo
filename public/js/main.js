df4bec04a1fca9f27fa6e107a3e1be32044874a9
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-04-05 23:44:37 -0400

    adding in btford's angular enhancements for socket.io.

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,41 @@
-var bingo = angular.module('bingo', ['ngRoute']);
+var socket = io.connect('http://localhost:3000');
+
+
+var bingo = angular.module('bingo', ['ngRoute', 'btford.socket-io'])
+  .factory('bingosockets', function(socketFactory) {
+    var myIoSocket = io.connect('http://localhost:3000');
+    var scks = socketFactory({
+      ioSocket: myIoSocket
+    });
+    scks.forward('error'); // makes all 'error' socket events avaliable as $scope.$on('socket:error', function(ev,data) {...};)
+    return scks;
+  });
 
 bingo.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl : '../pages/home.html',
-        })
-        .when('/new/cardset', {
-        	templateUrl : '../pages/addCardSet.html',
-        	controller : 'addCardSetController'
-        });
+  $routeProvider
+    .when('/', {
+      templateUrl: '../pages/home.html',
+    })
+    .when('/new/cardset', {
+      templateUrl: '../pages/addCardSet.html',
+      controller: 'addCardSetController'
+    });
 });
 
-bingo.controller('addCardSetController', function($scope, $http) {
-	$scope.formData = {};
-	$scope.msg = "";
+bingo.controller('addCardSetController', function($scope, $http, bingosockets) {
+  $scope.formData = {};
+  $scope.msg = "";
 
-	// Submit new page 
-	$scope.addCardSet = function () {
-		$http.post('/api/new/cardset', $scope.formData)
-			.success(function(data){
-				$scope.formData = {};
-				$scope.msg = "Congratulations! You have successfully added your card set!";
-			})
-			.error(function(data) {
-			console.log("Error: " + data);
-		});
-	};
+  // Submit new page
+  $scope.addCardSet = function() {
+    $http.post('/api/new/cardset', $scope.formData)
+      .success(function(data) {
+        $scope.formData = {};
+        $scope.msg = "Congratulations! You have successfully added your card set!";
+      })
+      .error(function(data) {
+        console.log("Error: " + data);
+      });
+  };
 
 });
-
