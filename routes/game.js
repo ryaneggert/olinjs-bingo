c2ad940ddf55c28a28e46ff15422ffe873eaeaa6
alias <email-blocked>
2015-04-07 16:48:10 -0400

    fixed indentation in game.js

--- routes/game.js
+++ routes/game.js
@@ -11,22 +11,22 @@ var routes = {};
 routes.newCardSet = function (req, res) {
 /* Create and save new card set based on user input */
 
-	// Get data submitted by user from form
-	var cardData = req.body.cards;
-	var name = req.body.name;
+  // Get data submitted by user from form
+  var cardData = req.body.cards;
+  var name = req.body.name;
 
-	// Create new CardSet object
-	var newCardSet = new CardSet({name: name, square_set: [cardData]});
+  // Create new CardSet object
+  var newCardSet = new CardSet({name: name, square_set: [cardData]});
 
-	// Save the new card set object to the database
-	newCardSet.save(function(err, cardset) {
-		if (err) {
-			console.error('Cant add new card set', err);
-			res.status(500).send("Couldn't add new card set to db");
-		}
-		console.log(cardset);
-		res.send(cardset);
-	});
+  // Save the new card set object to the database
+  newCardSet.save(function(err, cardset) {
+    if (err) {
+      console.error('Cant add new card set', err);
+      res.status(500).send("Couldn't add new card set to db");
+    }
+    console.log(cardset);
+    res.send(cardset);
+  });
 
 };
 
